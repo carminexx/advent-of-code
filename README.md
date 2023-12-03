0bdb95d49b0fdc3b8981c34b4d7907ce7b0d56e0
Carmine <carminexx@gmail.com>
2023-12-03 19:57:10 +0100

    [Day 2] Added solution also for part 2 of day 2 2023

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 |        | Language | Problem                                           | Solution                                                                                        | Part 1  | Part 2 |
 |--------|----------|---------------------------------------------------|-------------------------------------------------------------------------------------------------|---------|--------|
 | Day 1  | Rust     |[Problem 1](https://adventofcode.com/2023/day/1)   | [main.rs](https://github.com/carminexx/Advent-Of-Code/blob/develop/rust/2023/day-1/src/main.rs) | ✓       | ✓      |
-| Day 2  | Rust     |[Problem 1](https://adventofcode.com/2023/day/2)   | [main.rs](https://github.com/carminexx/Advent-Of-Code/blob/develop/rust/2023/day-2/src/main.rs) | ✓       | TODO   |
+| Day 2  | Rust     |[Problem 2](https://adventofcode.com/2023/day/2)   | [main.rs](https://github.com/carminexx/Advent-Of-Code/blob/develop/rust/2023/day-2/src/main.rs) | ✓       | ✓      |
+| Day 3  | Rust     |[Problem 3](https://adventofcode.com/2023/day/3)   | [main.rs](https://github.com/carminexx/Advent-Of-Code/blob/develop/rust/2023/day-3/src/main.rs) | TODO    | TODO   |
 
 
 ## 2022
--- rust/2023/day-2/src/main.rs
+++ rust/2023/day-2/src/main.rs
@@ -39,16 +39,63 @@ fn solve() -> u32 {
         .sum()
 }
 
+fn solve_part2() -> u32 {
+    fs::read_to_string("input.txt")
+        .unwrap()
+        .lines()
+        .map(|line| {
+            let (game_id, game_rounds) = line
+                .strip_prefix(INPUT_PREFIX)
+                .unwrap()
+                .split_once(':')
+                .unwrap();
+            let subsets = game_rounds.trim().split(';');
+
+            let mut max_cubes_in_a_game: HashMap<&str, u8> = HashMap::new();
+
+            subsets.for_each(|x| {
+                let cubes = x.trim().split(',');
+                cubes.for_each(|c| {
+                    let (value, color) = c.trim().split_once(' ').unwrap();
+
+                    max_cubes_in_a_game.insert(color, 
+                    match max_cubes_in_a_game.get(color) {
+                        Some(number) => {
+                            if number > &value.parse::<u8>().unwrap() {
+                                *number
+                            } else {
+                                value.parse::<u8>().unwrap()
+                            }
+                        },
+                        None => value.parse::<u8>().unwrap()
+                    });
+                })
+            });
+
+            // Calculate power of cubes
+
+            // Note: the cast to u32 is needed to avoid overflow when calculating the product, probably
+            // a cleaner solution by casting only the product itself and not all other elements is possible
+            max_cubes_in_a_game.iter().map(|x| u32::from(*x.1)).product::<u32>()
+            
+        })
+        .sum()
+}
+
 fn main() {
-    println!("{}", solve());
+    println!("Part one: {}", solve());
+    println!("Part two: {}", solve_part2());
 }
 
 #[cfg(test)]
 mod test {
+    use crate::solve_part2;
+
     use super::solve;
 
     #[test]
     fn solution() {
         assert_eq!(2447, solve());
+        assert_eq!(56322, solve_part2());
     }
 }

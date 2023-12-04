85d60c61aa034297848128bc289c6b796eeac5c2
Carmine <carminexx@gmail.com>
2023-12-04 17:05:23 +0100

    [Day 4] Added brute-force solution for part 2
    Took several seconds to run, can be improved for sure

--- rust/2023/day-4/src/main.rs
+++ rust/2023/day-4/src/main.rs
@@ -1,8 +1,8 @@
-use std::{collections::HashSet, fs, str::FromStr};
+use std::{collections::{HashSet, HashMap}, fs, str::FromStr};
 use anyhow::{anyhow, Context, Error, Result};
 use itertools::Itertools;
 
- #[derive(Debug)]
+ #[derive(Debug, Clone)]
  struct ScratchCard {
     id: u32,
     winning_numbers: HashSet<u8>,
@@ -36,12 +36,14 @@ impl FromStr for ScratchCard {
 }
 
 impl ScratchCard {
-    fn calculate_card_score(&self) -> u32 {
-        let scratched_winning_numbers = self.scratched_numbers.intersection(&self.winning_numbers).collect::<Vec<&u8>>();
+    fn scratched_winnings(&self) -> u8 {
+        self.scratched_numbers.intersection(&self.winning_numbers).count() as u8
+    }
 
-        match scratched_winning_numbers.len() {
+    fn calculate_card_score(&self) -> u32 {
+        match &self.scratched_winnings() {
             0 => 0,
-            _ => 2u32.pow(scratched_winning_numbers.len() as u32 - 1) as u32
+            _ => 2u32.pow(self.scratched_winnings() as u32 - 1) as u32
         }
     }
 }
@@ -59,8 +61,44 @@ fn solve_part1() -> u32 {
         .sum()
 }
 
+// Brute-force solution, adding each card to the list and counting the final score.
+// Tooks over 30-40 seconds to run, there's a lot of room for improvement for sure.
+fn solve_part2(input_source: &str) -> u32 {
+    let original_cards: HashMap<u32, ScratchCard> = fs::read_to_string(input_source)
+        .unwrap()
+        .lines()
+        .map(|line| {
+            line.parse::<ScratchCard>()
+                .context("failed to parse scratchcard")
+                .unwrap()
+        })
+        .map(|c| (c.id, c))
+        .collect();
+
+    let mut winned_cards: Vec<ScratchCard> = original_cards.iter().map(|x| x.1.clone()).collect::<Vec<_>>();
+    let mut score: u32 = 0;
+
+    while let Some(card) = winned_cards.pop() {
+        //winned_cards.push(card.clone()); // Put also fir
+        score += 1;
+        if card.scratched_winnings() == 0 {
+            continue;
+        }
+
+        for c in 1..=card.scratched_winnings() {
+            match original_cards.get(&(card.id + c as u32)) {
+                Some(c) => winned_cards.push(c.clone()),
+                None => continue
+            }
+        }
+    }
+    
+    score
+}
+
 fn main() {
     println!("Part one: {}", solve_part1());
+    println!("Part two: {}", solve_part2("input.txt"));
 }
 
 #[cfg(test)]
@@ -80,5 +118,15 @@ mod test {
     #[test]
     fn test_solve_part1() {
         assert_eq!(26914, solve_part1());
+    }
+
+    #[test]
+    fn test_demo_input_for_part2() {
+        assert_eq!(30, solve_part2("demo-input.txt"));
+    }
+
+    #[test]
+    fn test_solve_part2() {
+        assert_eq!(13080971, solve_part2("input.txt"));
     }
 }

81a5a8bbb8fcffc27d5ba468473e08414e1f3ee0
Carmine <carminexx@gmail.com>
2023-12-08 22:39:53 +0100

    [2022] [Day 3] Added solution for day 3 of 2022

--- rust/2022/day-3/src/main.rs
+++ rust/2022/day-3/src/main.rs
@@ -1,8 +1,7 @@
-use std::{fs::read_to_string, collections::HashSet};
+use std::{collections::HashSet, fs::read_to_string};
 
-fn main() {
-    let filename = "input.txt";
-    let priority_sum: i32 = read_to_string(filename) 
+fn solve_part1(input: &str) -> u32 {
+    read_to_string(input)
         .unwrap()
         .lines()
         .map(|x| {
@@ -10,14 +9,33 @@ fn main() {
             let elems_comp_a: HashSet<char> = comp_a.chars().collect();
             let elems_comp_b: HashSet<char> = comp_b.chars().collect();
 
-            // TODO
+            let mut unique = elems_comp_a.intersection(&elems_comp_b);
 
+            elem_to_priority(unique.next().unwrap())
         })
-        .sum();
+        .sum()
+}
 
-        println!("Sum of priorities: {}", priority_sum);
- }
+fn elem_to_priority(elem: &char) -> u32 {
+    let mut priorities = ('a'..='z').chain('A'..='Z');
+    priorities.position(|x| x == *elem).unwrap() as u32 + 1
+}
 
-fn elem_to_priority(elem) {
-    // TODO
+fn main() {
+    println!("Part 1: {}", solve_part1("input.txt"))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_demo_input_for_part_1() {
+        assert_eq!(157, solve_part1("demo-input.txt"));
+    }
+
+    #[test]
+    fn test_input_for_part_1() {
+        assert_eq!(8233, solve_part1("input.txt"));
+    }
 }

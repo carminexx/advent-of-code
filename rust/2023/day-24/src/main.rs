9334c429371ffe844eed67561feb4f3c111d5c07
Carmine <carminexx@gmail.com>
2023-12-26 22:30:15 +0100

    [Day 24] Added a second approach for part 1

--- rust/2023/day-24/src/main.rs
+++ rust/2023/day-24/src/main.rs
@@ -49,9 +49,9 @@ impl FromStr for Hail {
 }
 
 impl Hail {
-    fn intersect(&self, hail_2: &Hail) -> Option<(f64, f64)> {
+    fn intersect_2d_cramer(&self, hail_2: &Hail) -> Option<(f64, f64)> {
         /*
-            Simple intersection using matrix algebra and Cramer's rule
+            Approach A) Simple intersection using matrix algebra and Cramer's rule
 
             For example, considering the first Hail:
             19, 13, 30 @ -2,  1, -2
@@ -136,9 +136,79 @@ impl Hail {
 
         Some((x, y))
     }
+
+    fn intersect_2d_line_segments(&self, hail_2: &Hail) -> Option<(f64, f64)> {
+        /*
+           Approach B) Using numerical analysis to find the intersection point of two segments
+
+           This approach uses the algorithm proposed by Paul Bourke (see: https://paulbourke.net/geometry/pointlineplane/)
+           Here, instead of solving for the infinite line, only analyze the intersection point between two segments
+
+           The segments are calculated as: starting point + ending point; where the ending point is the position
+           of the Hail after a n number of nanoseconds passed.
+           The number of nanoseconds must be found by trial-and-error, since the problem doesn't state how much time passed.
+           With a value of 10, the solution works both for the sample input and my full input.
+           Higher values of n overflows the calculation of the ua parameter.
+
+           Algorithm:
+
+           Given two segments defined as (where P1..Pn are the x,y points coordinates):
+           Pa = P1 + ua ( P2 - P1 )
+           Pb = P3 + ub ( P4 - P3 )
+
+           Solving both for Pa = Pb (the intersection point):
+
+           x1 + ua (x2 - x1) = x3 + ub (x4 - x3)
+           y1 + ua (y2 - y1) = y3 + ub (y4 - y3)
+
+           Then, solve for either ua or ub, and input the resulting value in:
+
+           x = x1 + ua (x2 - x1)
+           y = y1 + ua (y2 - y1)
+        */
+
+        let elapsed_nanoseconds = 10;
+
+        let x1 = self.x;
+        let x2 = self.x + self.vx * elapsed_nanoseconds;
+
+        let x3 = hail_2.x;
+        let x4 = hail_2.x + hail_2.vx * elapsed_nanoseconds;
+
+        let y1 = self.y;
+        let y2 = self.y + self.vy * elapsed_nanoseconds;
+
+        let y3 = hail_2.y;
+        let y4 = hail_2.y + hail_2.vy * elapsed_nanoseconds;
+
+        let denominator = ((y4 - y3) * (x2 - x1)) - ((x4 - x3) * (y2 - y1));
+
+        if denominator == 0 {
+            return None;
+        }
+
+        let ua: f64 =
+            (((x4 - x3) * (y1 - y3)) - ((y4 - y3) * (x1 - x3))) as f64 / denominator as f64;
+
+        let x = x1 as f64 + (ua * (x2 - x1) as f64);
+        let y = y1 as f64 + (ua * (y2 - y1) as f64);
+
+        if ((x - self.x as f64).signum() != self.vx.signum() as f64)
+            || ((x - hail_2.x as f64).signum() != hail_2.vx.signum() as f64)
+        {
+            return None; // Collision happened in the past
+        }
+
+        Some((x, y))
+    }
 }
 
-fn solve_part1(input: &str, test_area_start: u64, test_area_end: u64) -> u16 {
+fn solve_part1(
+    input: &str,
+    test_area_start: u64,
+    test_area_end: u64,
+    intersect_method: fn(&Hail, &Hail) -> Option<(f64, f64)>,
+) -> u16 {
     let raw = fs::read_to_string(input).unwrap();
     let hails: Vec<Hail> = raw
         .lines()
@@ -154,17 +224,33 @@ fn solve_part1(input: &str, test_area_start: u64, test_area_end: u64) -> u16 {
     hails
         .iter()
         .combinations(2)
-        .map(|x| match x.first().unwrap().intersect(x.last().unwrap()) {
-            Some((x, y)) if test_range.contains(&x) && test_range.contains(&y) => 1,
-            _ => 0,
-        })
+        .map(
+            |x| match intersect_method(x.first().unwrap(), x.last().unwrap()) {
+                Some((x, y)) if test_range.contains(&x) && test_range.contains(&y) => 1,
+                _ => 0,
+            },
+        )
         .sum()
 }
 
 fn main() {
     println!(
-        "Part 1: {}",
-        solve_part1("input.txt", 200_000_000_000_000, 400_000_000_000_000)
+        "Part 1 with approach A: {}",
+        solve_part1(
+            "input.txt",
+            200_000_000_000_000,
+            400_000_000_000_000,
+            Hail::intersect_2d_cramer
+        )
+    );
+    println!(
+        "Part 1 with approach B: {}",
+        solve_part1(
+            "input.txt",
+            200_000_000_000_000,
+            400_000_000_000_000,
+            Hail::intersect_2d_line_segments
+        )
     );
 }
 
@@ -174,14 +260,40 @@ mod test {
 
     #[test]
     fn test_demo_input_for_part_1() {
-        assert_eq!(2, solve_part1("demo-input.txt", 7, 27));
+        assert_eq!(
+            2,
+            solve_part1("demo-input.txt", 7, 27, Hail::intersect_2d_cramer)
+        );
+    }
+
+    #[test]
+    fn test_demo_input_for_part_1_with_line_segments() {
+        assert_eq!(
+            2,
+            solve_part1("demo-input.txt", 7, 27, Hail::intersect_2d_line_segments)
+        );
     }
 
     #[test]
     fn test_solve_part_1() {
         assert_eq!(
             27328,
-            solve_part1("input.txt", 200_000_000_000_000, 400_000_000_000_000)
+            solve_part1(
+                "input.txt",
+                200_000_000_000_000,
+                400_000_000_000_000,
+                Hail::intersect_2d_cramer
+            )
+        );
+
+        assert_eq!(
+            27328,
+            solve_part1(
+                "input.txt",
+                200_000_000_000_000,
+                400_000_000_000_000,
+                Hail::intersect_2d_line_segments
+            )
         );
     }
 }
